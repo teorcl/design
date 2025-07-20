a775bf0a37834fb3190cb52ac7e8135ba260a564
Teodoro Calle Lara <teo.calle.lara@gmail.com>
2025-07-20 00:39:48 -0500

    feat(lib): Se crea carpeta src con pages y widgets, se agrega el header cuadrado

---
+++ lib/src/pages/headers_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/headers.dart';
+
+class HeadersPage extends StatelessWidget {
+  const HeadersPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: HeaderCuadrado());
+  }
+}
